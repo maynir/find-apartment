e3ecfeb3163e608fdb9eb290b9b5934d98b5a5eb
May Nir <maynirrr@gmail.com>
2022-01-23 01:26:13 +0200

    add groups

--- scrape_with_sel.py
+++ scrape_with_sel.py
@@ -46,7 +46,7 @@ EMAIL = "maynirrr@gmail.com"
 EMAIL_FOR_SEND = "findapartmenttlv@gmail.com"
 PASSWORD = ""
 
-group_ids = [35819517694]
+group_ids = [35819517694, 101875683484689, 174312609376409, 2092819334342645, 1673941052823845]
 
 browser = webdriver.Chrome(ChromeDriverManager().install(), options=option)
 browser.get("http://facebook.com")
@@ -57,76 +57,79 @@ email_field.send_keys(EMAIL)
 pass_field = wait.until(EC.visibility_of_element_located((By.NAME, 'pass')))
 pass_field.send_keys(PASSWORD)
 pass_field.send_keys(Keys.RETURN)
-time.sleep(5)
-
-# once logged in, free to open up any target page
-browser.get(f'https://www.facebook.com/groups/35819517694?sorting_setting=CHRONOLOGICAL')
-time.sleep(5)
-
-# while True:
-browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-time.sleep(5)
-browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-time.sleep(5)
+time.sleep(10)
 
 new_apartments_count = 0
 
-posts = browser.find_elements_by_xpath("//*[@class='du4w35lb k4urcfbm l9j0dhe7 sjgh65i0']")
-for post in posts:
-  see_mores = post.find_elements_by_xpath(".//div[contains(text(), 'See more')]")
-  for see_more in see_mores:
-    try:
-        ActionChains(browser).move_to_element(see_more).perform()
-        see_more.click()
-        print('Load More clicked')
-        time.sleep(1)
-    except:
-        print('not clicked')
-        continue
-  id = post.find_element_by_tag_name("strong").text
-  
-  try:
-      if id in seen_apartments:
-          print("Apartment id: " + id + ", seen already.")
-          print("__________________________")
-          continue
+for group_id in group_ids:
+    # once logged in, free to open up any target page
+    browser.get(f'https://www.facebook.com/groups/{group_id}?sorting_setting=CHRONOLOGICAL')
+    time.sleep(5)
 
+    # while True:
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
+
+    posts = browser.find_elements_by_xpath("//*[@class='du4w35lb k4urcfbm l9j0dhe7 sjgh65i0']")
+    for post in posts:
+      see_mores = post.find_elements_by_xpath(".//div[contains(text(), 'See more')]")
+      for see_more in see_mores:
+        try:
+            ActionChains(browser).move_to_element(see_more).perform()
+            see_more.click()
+            print('Load More clicked')
+            time.sleep(1)
+        except:
+            print('not clicked')
+            continue
+      id = post.find_element_by_tag_name("strong").text
+      
       try:
-          text = post.find_element_by_xpath(".//div[@class='ecm0bbzt hv4rvrfc ihqw7lf3 dati1w0a']").text
-      except:
-          text = post.find_element_by_xpath(".//div[@class='kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql ii04i59q']").text
-      posted_by = id
-      posted_by_url = post.find_elements_by_xpath(".//a")[0].get_attribute('href')
-      post_url = post.find_elements_by_xpath(".//a")[1].get_attribute('href')
+          if id in seen_apartments:
+              print("Apartment id: " + id + ", seen already.")
+              print("__________________________")
+              continue
 
-      mycol.insert_one({"apartment_id": id, "posted_by": posted_by,
-                        "posted_by_url": posted_by_url, "post_url": post_url, "text": text})
+          try:
+              text = post.find_element_by_xpath(".//div[@class='ecm0bbzt hv4rvrfc ihqw7lf3 dati1w0a']").text
+          except:
+              text = post.find_element_by_xpath(".//div[@class='kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql ii04i59q']").text
+          posted_by = id
+          posted_by_url = post.find_elements_by_xpath(".//a")[0].get_attribute('href')
+          post_url = post.find_elements_by_xpath(".//a")[1].get_attribute('href')
 
-      if not regex.search(text):
-          print("Apartment not matching words")
+          mycol.insert_one({"apartment_id": id, "posted_by": posted_by,
+                            "posted_by_url": posted_by_url, "post_url": post_url, "text": text})
+
+          if not regex.search(text):
+              print("Apartment not matching words")
+              print("post text: " + text)
+              print("__________________________")
+              continue
+
+          new_apartments_count += 1
+          mail_content += f'Apartment number {new_apartments_count}:\nPost text: \n{text}\nPost link: \n{post_url}\nPosted by: \n{posted_by}\nPosted by URL: \n{posted_by_url}\n\n\n'
+
           print("post text: " + text)
+          print("post link: " + post_url)
+          print("posted by: " + posted_by)
+          print("user url: " + posted_by_url)
           print("__________________________")
-          continue
+      except Exception as err:
+          mycol.insert_one({"apartment_id": id})
+          print(f'couldnt parse apartment_id: {id}, msg: {err}')
+          print("__________________________")
 
-      new_apartments_count += 1
-      mail_content += f'Apartment number {new_apartments_count}:\nPost text: \n{text}\nPost link: \n{post_url}\nPosted by: \n{posted_by}\nPosted by URL: \n{posted_by_url}\n\n\n'
-
-      print("post text: " + text)
-      print("post link: " + post_url)
-      print("posted by: " + posted_by)
-      print("user url: " + posted_by_url)
-      print("__________________________")
-  except Exception as err:
-      mycol.insert_one({"apartment_id": id})
-      print(f'couldnt parse apartment_id: {id}, msg: {err}')
-      print("__________________________")
+    time.sleep(15)
 
 if(new_apartments_count > 0):
     # Setup the MIME
     message = MIMEMultipart()
     message['From'] = EMAIL_FOR_SEND
     message['To'] = EMAIL
-    message['Subject'] = 'New apartment alert'  # The subject line
+    message['Subject'] = f'New {new_apartments_count} apartments alert'  # The subject line
     # The body and the attachments for the mail
     message.attach(MIMEText(mail_content, 'plain'))
     # Create SMTP session for sending the mail
@@ -141,5 +144,4 @@ if(new_apartments_count > 0):
 new_apartments_count = 0
 mail_content = ""
 
-# time.sleep(60*10)
 browser.quit()
