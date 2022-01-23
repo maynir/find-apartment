b4e0ca5ccda10dbd35e20b1459a09a9db0d3abd3
May Nir <maynirrr@gmail.com>
2022-01-23 17:57:59 +0200

    add groups

--- scrape_with_sel.py
+++ scrape_with_sel.py
@@ -35,7 +35,7 @@ mydb = myclient["apartmentsdb"]
 mycol = mydb["apartments"]
 
 words = ["השותף", "השותפה", "שותף", "שותפה", "מתפנה חדר", "מפנה חדר", "מחליפה", "מחליף", "מחליפ/ה", "שותפ/ה", "מחפשת שותפה", "מחפש שותפה", "מחפש שותף",
-         "מחפשת שותפה", "מפנה את החדר שלי", "חדר להשכרה", "דירת שותפים", "בדירת שותפים", "מפנה את חדרי", "שותף/ה", "עוזב את החדר שלי", "עוזבת את החדר שלי", "חדר בדירת"]
+         "מחפשת שותפה", "מפנה את החדר שלי", "חדר להשכרה", "דירת שותפים", "בדירת שותפים", "מפנה את חדרי", "שותף/ה", "עוזב את החדר שלי", "עוזבת את החדר שלי", "חדר בדירת", "שותפים", "שותפות", "מפנה את החדר"]
 regex = re.compile('|'.join(re.escape(x) for x in words))
 
 # set options as you wish
@@ -47,10 +47,10 @@ option.add_argument("--disable-notifications")
 
 EMAIL = "maynirrr@gmail.com"
 EMAIL_FOR_SEND = "findapartmenttlv@gmail.com"
-PASSWORD = ""
+PASSWORD = "M20y23Nir"
 
-group_ids = [35819517694, 101875683484689, 174312609376409, 2092819334342645, 1673941052823845, 599822590152094]
-group_id_to_sorting = {35819517694: 'CHRONOLOGICAL', 101875683484689: 'CHRONOLOGICAL', 174312609376409: 'CHRONOLOGICAL', 2092819334342645: 'CHRONOLOGICAL', 1673941052823845: 'CHRONOLOGICAL', 599822590152094: 'RECENT_LISTING_ACTIVITY'}
+group_ids = [35819517694, 101875683484689, 174312609376409, 2092819334342645, 1673941052823845, 599822590152094, 287564448778602, 'ApartmentsTelAviv', 785935868134249, 423017647874807, 458499457501175, 108784732614979, 'telavivroommates', 109472732403520]
+group_id_to_sorting = {35819517694: 'CHRONOLOGICAL', 101875683484689: 'CHRONOLOGICAL', 174312609376409: 'CHRONOLOGICAL', 2092819334342645: 'CHRONOLOGICAL', 1673941052823845: 'CHRONOLOGICAL', 599822590152094: 'RECENT_LISTING_ACTIVITY', 287564448778602: 'RECENT_LISTING_ACTIVITY', 'ApartmentsTelAviv': 'CHRONOLOGICAL', 785935868134249: 'RECENT_LISTING_ACTIVITY', 423017647874807: 'CHRONOLOGICAL', 458499457501175: 'RECENT_LISTING_ACTIVITY', 108784732614979: 'CHRONOLOGICAL', 'telavivroommates': 'RECENT_LISTING_ACTIVITY', 109472732403520: 'CHRONOLOGICAL'}
 
 browser = webdriver.Chrome(ChromeDriverManager().install(), options=option)
 browser.get("http://facebook.com")
@@ -79,14 +79,32 @@ while True:
       browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
       time.sleep(random_num(5,7))
       browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-      time.sleep(random_num(5,7))
+      time.sleep(random_num(8,10))
 
       posts = browser.find_elements_by_xpath("//*[@class='du4w35lb k4urcfbm l9j0dhe7 sjgh65i0']")
+      if(len(posts) == 0 or len(posts) == 1):
+        print(f"Found {len(posts)} posts, refreshing page...")
+        browser.refresh()
+        time.sleep(random_num(5,7))
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(random_num(5,7))
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(random_num(8,10))
+        posts = browser.find_elements_by_xpath("//*[@class='du4w35lb k4urcfbm l9j0dhe7 sjgh65i0']")
+
+      time.sleep(random_num(8,10))
       print(f"Found {len(posts)} posts in group")
       print("__________________________")
 
       for post in posts:
+        ActionChains(browser).move_to_element(post).perform()
+        time.sleep(1)
         id = post.find_element_by_tag_name("strong").text
+
+        if (id == ''):
+            print("id is empty, trying again")
+            time.sleep(2)
+            id = post.find_element_by_xpath(".//a[@class='oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gpro0wi8 oo9gr5id lrazzd5p']").text
 
         if id in seen_apartments:
             print("Apartment id: " + id + ", seen already.")
@@ -111,27 +129,30 @@ while True:
                 text = post.find_element_by_xpath(".//div[@class='kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql ii04i59q']").text
             posted_by = id
             posted_by_url = post.find_element_by_xpath(".//a[@class='oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gpro0wi8 oo9gr5id lrazzd5p']").get_attribute('href')
-            post_url = post.find_element_by_xpath(".//a[@class='oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 gpro0wi8 b1v8xokw']").get_attribute('href')
-            posted_ago = post.find_element_by_xpath(".//a[@class='oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 gpro0wi8 b1v8xokw']").find_element_by_tag_name("span").text
+            # post_url = post.find_element_by_xpath(".//a[@class='oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 gpro0wi8 b1v8xokw']").get_attribute('href')
+            ActionChains(browser).move_to_element(post).perform()
+            time.sleep(1)
+            # posted_ago = post.find_element_by_xpath(".//span[@class='tojvnm2t a6sixzi8 abs2jz4q a8s20v7p t1p8iaqh k5wvi7nf q3lfd5jv pk4s997a bipmatt0 cebpdrjk qowsmv63 owwhemhu dp1hu0rb dhp61c6y iyyx5f41']").text
             
-            match = regex.search(text)
+            match = bool(regex.search(text))
             
             mycol.insert_one({"apartment_id": id, "posted_by": posted_by,
-                              "posted_by_url": posted_by_url, "post_url": post_url, "text": text, "posted_ago": posted_ago, "group_name": group_name, "group_url": group_url, "match": match })
+                              "posted_by_url": posted_by_url, "text": text, "group_name": group_name, "group_url": group_url, "match": match })
 
             if not match:
                 print("Apartment not matching words")
+                print(f"post id: {id}")
                 print("post text: " + text)
                 print("__________________________")
                 continue
 
             print("!!!MATCH!!!")
             new_apartments_count += 1
-            mail_content += f'Apartment number {new_apartments_count}:\nPosted {posted_ago} ago\nPost text: \n{text}\nPost link: \n{post_url}\nPosted by: \n{posted_by}\nPosted by URL: \n{posted_by_url}\nGroup name: \n{group_name}\nGroup URL: \n{group_url}\n\n\n\n'
+            mail_content += f'Apartment number {new_apartments_count}:\nPost text: \n{text}\nPosted by: \n{posted_by}\nPosted by URL: \n{posted_by_url}\nGroup name: \n{group_name}\nGroup URL: \n{group_url}\n\n\n\n'
 
             print("post text: " + text)
-            print(f"post {posted_ago} ago")
-            print("post link: " + post_url)
+            # print(f"post {posted_ago} ago")
+            # print("post link: " + post_url)
             print("posted by: " + posted_by)
             print("user url: " + posted_by_url)
             print(f"posted at: {group_name}")
@@ -141,30 +162,31 @@ while True:
             print(f'couldnt parse apartment_id: {id}, msg: {err}')
             print("__________________________")
 
+      if(new_apartments_count > 0):
+          # Setup the MIME
+          message = MIMEMultipart()
+          message['From'] = EMAIL_FOR_SEND
+          message['To'] = EMAIL
+          message['Subject'] = f'New {new_apartments_count} apartments for you'  # The subject line
+          # The body and the attachments for the mail
+          message.attach(MIMEText(mail_content, 'plain'))
+          # Create SMTP session for sending the mail
+          session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
+          session.starttls()  # enable security
+          session.login(EMAIL_FOR_SEND, PASSWORD)  # login with mail_id and password
+          text = message.as_string()
+          session.sendmail(EMAIL_FOR_SEND, EMAIL, text)
+          session.quit()
+          print(f'Mail Sent with {new_apartments_count} new apartments')
+      else:
+          print('No new apartments...')
+
+      new_apartments_count = 0
+      mail_content = ""
+
       print("Done with group")
+      print("__________________________\n")
       time.sleep(random_num(13,15))
-
-  if(new_apartments_count > 0):
-      # Setup the MIME
-      message = MIMEMultipart()
-      message['From'] = EMAIL_FOR_SEND
-      message['To'] = EMAIL
-      message['Subject'] = f'New {new_apartments_count} apartments for you'  # The subject line
-      # The body and the attachments for the mail
-      message.attach(MIMEText(mail_content, 'plain'))
-      # Create SMTP session for sending the mail
-      session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
-      session.starttls()  # enable security
-      session.login(EMAIL_FOR_SEND, PASSWORD)  # login with mail_id and password
-      text = message.as_string()
-      session.sendmail(EMAIL_FOR_SEND, EMAIL, text)
-      session.quit()
-      print(f'Mail Sent with {new_apartments_count} new apartments')
-  else:
-      print('No new apartments :(')
-
-  new_apartments_count = 0
-  mail_content = ""
 
   print("Sleeping for 10 minutes now...")
   time.sleep(60*10) # Wait 10 minutes before starting all over
